55773accd9f4262e2edffd3c1410cfbc95739a94
jegors jeskovs <jegorsj@gmail.com>
2019-10-16 22:51:16 -0600

    Made the app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hate Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Loathe Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Detest Burger", true);
